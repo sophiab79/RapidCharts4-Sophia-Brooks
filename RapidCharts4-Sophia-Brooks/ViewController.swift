b4e3f4148fb9dd817ab53949f0692888225a8574
sophiab79 <sophiabrooks998@gmail.com>
2020-12-14 11:34:49 -0800

    Initial Commit

---
+++ RapidCharts4-Sophia-Brooks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RapidCharts4-Sophia-Brooks
+//
+//  Created by Sophia on 12/14/20.
+//  Copyright © 2020 Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
