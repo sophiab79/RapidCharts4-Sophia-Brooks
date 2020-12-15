c5163dc41d94beacab0df713057a5f70c51a31dc
sophiab79 <sophiabrooks998@gmail.com>
2020-12-14 17:25:24 -0800

    rapid charts with table view

---
+++ RapidCharts4-Sophia-Brooks/ToDetailTableViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ToDetailTableViewController.swift
+//  RapidCharts4-Sophia-Brooks
+//
+//  Created by Sophia on 12/14/20.
+//  Copyright © 2020 Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ToDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var noteView: UITextView!
+    
+    var toDoItem: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if toDoItem == nil {
+            toDoItem = ""
+            
+        }
+        
+        nameField.text = toDoItem
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+    
+    
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text 
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+        
+    }
+    
+    
+
+    // MARK: - Table view data source
+
+    //override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+   //     return 0
+  //  }
+
+  //  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+   //     return 0
+    }
+
+//}
---
+++ RapidCharts4-Sophia-Brooks/ToDoListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  RapidCharts4-Sophia-Brooks
+//
+//  Created by Sophia on 12/14/20.
+//  Copyright © 2020 Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+  
+    var toDoArray = ["Learn Swift", "Build Apps", "Change the World", "Take a Vacation"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let destination = segue.destination as! ToDetailTableViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.toDoItem = toDoArray[selectedIndexPath.row]
+            
+        } else {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
+    }
+    @IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
+ let source = segue.source as! ToDetailTableViewController
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            toDoArray[selectedIndexPath.row] = source.toDoItem
+            tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
+        } else {
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(source.toDoItem)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+            tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+            
+        }
+        }
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    print("numberOfRowsInSection was just called. Returning \(toDoArray.count)")
+    return toDoArray.count
+    
+}
+    
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoArray[indexPath.row])")
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    cell.textLabel?.text = toDoArray[indexPath.row]
+    return cell
+    
+}
+}
+
